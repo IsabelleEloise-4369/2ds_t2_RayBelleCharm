1dd1a47eb5c65da1b5d44e65c5a35713c65d78ec
Jheniffer Rayane de Andrade <jheniffer.andrade4325@gmail.com>
2024-05-09 17:18:17 -0300

    criação da rota usuario e estrutura do cadastro

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask , render_template, request, redirect, session, jsonify
+from usuario import Usuario
 
 #app é o servidor
 #criei o objeto app usando a classe Flask
@@ -10,9 +11,31 @@ app.secret_key = 'raybelle'
 def pagina_inicial():
     return render_template("raybelle.html")
 
-@app.route("/cadastro")
+@app.route("/cadastro", methods=["GET", "POST"])
 def pagina_cadastro():
-    return render_template("cadastro-raybelle.html")
+    if request.method == "GET":
+        return render_template("cadastro-raybelle.html")
+    if request.method == "POST":
+        # Extrair os dados do formulário
+        nome = request.form.get("nome")
+        cpf = request.form.get("cpf")
+        email = request.form.get("email")
+        telefone = request.form.get("telefone")
+        senha = request.form.get("senha")
+        enderecoRua = request.form.get("enderecoRua")
+        enderecoNumero = request.form.get("enderecoNumero")
+        estado = request.form.get("estado")
+        cidade = request.form.get("cidade")
+        
+        # Criar uma instância da classe Usuario
+        usuario = Usuario()
+
+        # Chamar o método cadastrar
+        if usuario.cadastrar(nome, cpf, email, telefone, senha, enderecoRua, enderecoNumero, estado, cidade):
+            return redirect("/")  # Redirecionar para a página inicial após o cadastro
+        else:
+            return "Erro ao cadastrar o usuário."
+
 
 @app.route("/login")
 def pagina_logn():
--- usuario.py
+++ usuario.py
@@ -1,4 +1,5 @@
 from conexao import Conexao
+from hashlib import sha256
 
 class Usuario:
     def __init__ (self):
@@ -11,3 +12,29 @@ class Usuario:
         self.enderecoNumero = None
         self.estado = None
         self.cidade = None
+
+# criando a função para cadastrar um usuário
+    def cadastrar (self, nome, cpf, email, telefone, senha, enderecoRua, enderecoNumero, estado, cidade):
+        #criptografar a senha
+        senha = sha256(senha.encode()).hexdigest()
+
+        # try:
+        # conectando o banco de dados
+        mydb = Conexao.conectar()
+
+        mycursor = mydb.cursor()
+    
+        #inserir os dados na tabela 
+        dados = f"INSERT INTO tb_cliente (nome, cpf, email, telefone, senha, endereco_rua, endereco_numero, estado, cidade) VALUES ('{nome}', '{cpf}', '{email}', '{telefone}', '{senha}', '{enderecoRua}', '{enderecoNumero}', '{estado}', '{cidade}')"
+
+        #executar
+        mycursor.execute(dados)
+
+        mydb.commit()
+
+        mydb.close()
+
+        return True
+
+        # except:
+            # return False
