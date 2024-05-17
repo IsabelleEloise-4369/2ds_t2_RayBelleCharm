8579b719fa3f96e6471dd5f4450fb982000641a0
Isabelle Eloise Rugno Ferreira <Isabelle.eloise4369@gmail.com>
2024-05-17 08:02:49 -0300

    atualizações

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template, request, redirect, session, jsonify
+from flask import Flask, render_template, request, redirect, session, jsonify
 from usuario import Usuario
 
 #app é o servidor
