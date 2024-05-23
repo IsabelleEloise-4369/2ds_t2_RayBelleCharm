a50c4f632a4d83759139c0928b381f9e2070371d
Jheniffer Rayane de Andrade <jheniffer.andrade4325@gmail.com>
2024-05-23 14:47:57 -0300

    alteracaoEstrutura

--- conexao.py
+++ conexao.py
@@ -3,6 +3,8 @@ class Conexao:
     def conectar():
         mydb = mysql.connector.connect(
         host ="127.0.0.1",
+        # não esquecer de modificar isso conforme a porta do banco de dados
+        port = "3307",
         user ="usuario_raybelle",
         password ="raybelle",
         database ="raybellecharm")
